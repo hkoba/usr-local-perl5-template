6f54b40630af7ac3b958fd9bc883734c3febb4c4
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-11-30 15:11:05 +0900

    run-cpm.zsh - install cpm from `https://git.io/cpm` and run install -g

---
+++ run-cpm.zsh
@@ -0,0 +1,93 @@
+#!/bin/zsh
+
+emulate -L zsh
+
+error=0
+
+#========================================
+
+# /usr/local
+binDir=$(cd $0:h && print $PWD)
+
+# /usr/local/perl5/bin
+copiedBin=$binDir/bin
+
+# /usr/local/bin
+localBin=$binDir:h/bin
+
+cpm=$localBin/cpm
+
+sudo=()
+cpm_sudo=()
+if ((UID != 0)); then
+    sudo=(sudo)
+    cpm_sudo=(--sudo)
+fi
+
+#========================================
+
+zparseopts -D -K n=o_dryrun v=o_verbose R=o_rebuild y=o_yes
+
+function x {
+    print "# $@"
+    if (($#o_dryrun)); then return; fi
+    "$@" || exit $?
+}
+
+function xx {
+    print "# $@"
+    "$@" || exit $?
+}
+
+#========================================
+
+cd $binDir
+
+[[ -x $cpm ]] || o_rebuild=(-R)
+
+if (($#o_rebuild)); then
+    o_changes=()
+else
+    o_changes=(-c)
+fi
+
+if (($#o_rebuild)); then
+
+    x $binDir/symlinks.zsh record
+
+    x $sudo rm -rf $binDir/lib64/perl5/auto/**/*.(so|bs)(N)
+    x $sudo rm -rf $binDir/{lib64,share}/perl5/*(N)
+
+    xx $sudo $binDir/dnf-install.zsh $o_yes $o_dryrun
+fi
+
+if (($#o_rebuild)); then
+    x curl -fsSL --compressed --output $copiedBin/cpm https://git.io/cpm
+    x $sudo chmod +x $copiedBin/cpm
+fi
+
+# cpm の symlink を確実にする
+if [[ ! -L $localBin/cpm ]]; then
+    x $sudo ln -vnsfr $copiedBin/cpm $localBin/cpm
+fi
+
+if (($#o_rebuild)); then
+    xx $sudo $binDir/make-symlink.zsh $o_dryrun
+fi
+
+# 予め入れておかないと cpm による install がコケるものを列挙
+if (($#o_rebuild)); then
+    prereqFn=prereq.lst
+    if [[ -r $prereqFn ]]; then
+        x $cpm install $cpm_sudo -g $(< $prereqFn) || exit 1
+    fi
+fi
+
+x $cpm install $cpm_sudo -g "$@" || error=1
+
+if ((error)); then
+    echo cpm failed
+    exit 1
+else
+    echo DONE
+fi
---
+++ symlinks.zsh
@@ -0,0 +1,134 @@
+#!/bin/zsh
+
+#----------------------------------------
+# Reset options
+# zsh の動作オプションを揃える
+
+emulate -L zsh
+
+set -eu
+
+# setopt YOUR_FAVORITE_OPTIONS_HERE
+
+setopt extended_glob
+
+#----------------------------------------
+# Set variables for application directory
+# スクリプトをインストール個所に依存させないための変数を用意
+
+# $0            = ~/bin/mytask
+# $realScriptFn = /somewhere/myapp/bin/myscript.zsh
+# binDir        = /somewhere/myapp/bin
+# appDir        = /somewhere/myapp
+
+realScriptFn=$(readlink -f $0); # macOS/BSD の人はここを変更
+binDir=$realScriptFn:h
+# appDir=$binDir:h
+
+#----------------------------------------
+# Parse primary options
+# オプションの解析
+
+o_dryrun=() o_quiet=() o_xtrace=() o_help=()
+o_version=()
+
+zparseopts -D -K \
+           n=o_dryrun -dry-run=o_dryrun \
+           q=o_quiet -quiet=o_quiet \
+           x=o_xtrace \
+           h=o_help      -help=o_help \
+           -version:=o_version
+
+if (($#o_xtrace)); then set -x; fi
+
+#----------------------------------------
+# Utility functions
+# いつも使う関数をここで。(source しても良い)
+
+function x {
+    if (($#o_dryrun || !$#o_quiet)); then
+        print -R '#' ${(q-)argv}
+    fi
+    if (($#o_dryrun)); then
+        return;
+    fi
+    "$@" || exit $?
+}
+
+function die { echo 1>&2 $*; exit 1 }
+
+#----------------------------------------
+# Define subcommands here
+# サブコマンドはここで定義する
+
+function cmd_version {
+    if (($#o_version)); then
+        print ${o_version[2]#=}
+    else
+        /usr/bin/perl -MConfig -le 'print $Config{PERL_VERSION}'
+    fi
+}
+
+function cmd_record {
+    local ver; ver=$(cmd_version)
+
+    if (($#o_dryrun)); then
+        die "Dry-run モードは未実装です"
+    fi
+
+    local kind dn
+    for kind in share lib64; do
+        dn=$kind/perl5/5.$ver
+        [[ -d $dn ]] || {
+            echo 1>&2 $dn は存在しないのでスキップします
+            continue
+        }
+        (cd $dn && find -type l |
+                 xargs perl -e '
+                   print join "\t", $_, qx(readlink -f $_) for @ARGV
+                 '
+        ) > $binDir/symlinks_$kind.lst
+    done
+}
+
+function cmd_link {
+    local ver; ver=$(cmd_version)
+
+    cd $binDir
+
+    local kind fn destPrefix realDest dest src
+    for kind in share lib64; do
+        destPrefix=$kind/perl5/5.$ver
+        for dest src in $(< $binDir/symlinks_$kind.lst); do
+            realDest=$destPrefix/$dest
+            [[ -d $realDest:h ]] || x mkdir -p $realDest:h
+            x ln -vnsfr $src $realDest || break
+        done
+    done
+}
+
+function cmd_help {
+    if ((ARGC)); then
+        echo 1>&2 $*
+    fi
+    cat 1>&2 <<EOF
+Usage: ${realScriptFn:t} [-h] [-n] SUBCOMMAND ...
+EOF
+    exit 1
+}
+
+#----------------------------------------
+# Finally, dispatch given subcommand (or show help)
+# 実際の実行
+
+if (($#o_help)) || ! ((ARGC)); then
+    cmd_help "$@"
+fi
+
+cmd=$1; shift
+
+if (($+functions[cmd_$cmd])); then
+    cmd_$cmd "$@"
+else
+    cmd_help "No such subcommand: $cmd"
+fi
