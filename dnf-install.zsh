f91b8360362c7f26f7711e685ade95aa853bdd67
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-11-30 22:46:39 +0900

    Add non-perl rpm installation support

--- dnf-install.zsh
+++ dnf-install.zsh
@@ -2,8 +2,12 @@
 
 emulate -L zsh
 
-dataFile=${0:r}-perlmodules.lst
+modListFile=${0:r}-perlmodules.lst
+rpmListFile=${0:r}-rpm.lst
 
-modList=($(grep -v '^#' $dataFile))
+modList=($(grep -v '^#' $modListFile)) || exit 1
+pkgList=("perl("$^modList")")
 
-dnf install "$@" "perl("$^modList")"
+pkgList+=($(grep -v '^#' $rpmListFile)) || exit 1
+
+dnf install "$@" $pkgList
